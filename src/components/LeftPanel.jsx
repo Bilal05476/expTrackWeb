fe4bddc4e732f9d9227a88b00dcd3561323b90ab
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-28 15:25:43 +0500

    updates

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,22 +1,22 @@
 import "../css/leftPanel.css";
+import logo from "../img/trackerlogo.png";
+import wallet from "../img/wallet-icon.png";
 
 const LeftPanel = () => {
   return (
     <div className="left-panel">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nostrum
-      delectus praesentium. Impedit excepturi officiis eaque et soluta quaerat
-      mollitia ratione qui non quos vero minus dolores sunt numquam, animi nisi
-      autem, molestias ad. Dignissimos nisi, quisquam minus beatae enim neque
-      eligendi rerum nesciunt maxime illum dolorum officiis dicta vero culpa
-      architecto nihil recusandae maiores minima cumque reprehenderit soluta
-      quas voluptates voluptatibus? Perspiciatis eveniet pariatur sed cum
-      repellat incidunt in, eos excepturi aliquid, at eum amet consequuntur ipsa
-      debitis rem. Officia similique possimus dolor dolores est. Natus sunt
-      earum reiciendis sint dignissimos error corrupti at modi iste! Laboriosam
-      maiores quis corporis nesciunt dolorum repellat, nam, consequatur
-      inventore, laborum autem nostrum vero? Molestiae reiciendis earum
-      voluptatem accusamus ipsa upiditate nesciunt id sit illo, delectus ut
-      omnis aspernatur quidem?
+      <div className="expense-tracker-heading">
+        <img src={logo} alt="logo" />
+        <h4>Expense Tracker</h4>
+      </div>
+      <div className="user-account-details">
+        <img src={logo} alt="profile" />
+        <h5 className="userName my-2">UserName</h5>
+        <div className="wallet-amount my-2">
+          <img src={wallet} alt="wallet" />
+          <p className="m-0">$ 5,240</p>
+        </div>
+      </div>
     </div>
   );
 };
