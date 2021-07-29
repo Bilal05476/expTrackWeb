8954d6ad376b68b3856e329c36147336ce3019a6
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-29 22:13:26 +0500

    ui done

--- src/components/ExpenseChart.jsx
+++ src/components/ExpenseChart.jsx
@@ -4,10 +4,10 @@ import { Animation, EventTracker, PieSeries } from "@devexpress/dx-react-chart";
 import { useContext } from "react";
 import { GlobalContext } from "../Context/GlobalState";
 
-const data = [
-  { expense: "picnic", val: 101 },
-  { expense: "studies", val: 72 },
-];
+// const data = [
+//   { expense: "picnic", val: 101 },
+//   { expense: "studies", val: 72 },
+// ];
 
 export default function ExpenseChart() {
   const { userTransactions } = useContext(GlobalContext);
